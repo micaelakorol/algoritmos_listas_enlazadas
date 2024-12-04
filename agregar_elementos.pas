39deec8ff956be9c76af9568ca88509c37348809
Micaela <micaelakorol@gmail.com>
2024-12-04 10:18:15 -0300

    algoritmos

---
+++ agregar_elementos.pas
@@ -0,0 +1,29 @@
+Program agregar_adelante
+
+Type list =  ^dato;
+
+dato = record
+elemento:integer;
+siguiente: list;
+
+procedure agregarAdelante(var pI:list; num:integer)
+var 
+ nuevo:list
+ begin
+  new(nuevo); nuevo^.dato := num; nuevo^.sig := nil;
+  if (pI = nil) then pI := nuevo;
+  else
+   begin
+    nuevo^.sig := pI;
+    pI := nuevo; 
+   end
+ end
+
+var
+pI:list;
+num:integer;
+begin
+num := 10;
+pI:= nil;
+agregarAdelante(pI, num)
+end.
---
+++ crear_lista.pas
@@ -0,0 +1,14 @@
+Program crear_lista_integer
+
+Type list =  ^dato;
+
+dato = record
+elemento:integer;
+siguiente: list;
+
+var
+pI:list
+
+begin
+pI:= nil;
+end.
